a1eab7d9e230f9369d6873dcf4d96868f00eca74
Kareem Shehab <kshehab1@student.gsu.edu>
2023-03-18 19:00:57 -0700

    adding user auth with JWT based on just a username

--- Task Tracker/task-tracker-frontend/functions/createUser.js
+++ Task Tracker/task-tracker-frontend/functions/createUser.js
@@ -1,30 +1,51 @@
-const { PrismaClient, PrismaClientRequestError } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient, Prisma } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.handler = async (event, context, callback) => {
   try {
-    const data = JSON.parse(event.body)
-    const createdUser = await prisma.user.create({ data })
+    const createdUser = await createUserInDB(event);
 
+    // Create token with user information
+    const tokenSecret = process.env.TOKEN_SALT;
+    const token = jwt.sign({ uid: user.id }, tokenSecret, {
+      algorithm: "RS256",
+      expiresIn: "1m",
+    });
+
+    // return user info back to client req, with new bearer token included
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(createdUser)
-    }
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(createdUser),
+    };
   } catch (e) {
-    if (e instanceof PrismaClientRequestError) {
-      if (e.code === 'P2002') {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
         return {
           statusCode: 409,
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            error: 'A user with this email already exists'
-          })
-        }
+            error: "A user with this username already exists",
+          }),
+        };
       }
     }
 
-    console.error(e)
-    return { statusCode: 500 }
+    return { statusCode: 500, body: JSON.stringify(e) };
   }
+};
+
+async function createUserInDB({ body }) {
+  const data = JSON.parse(body);
+  const createdUser = await prisma.users.create({
+    data: { count: 0, username: data.username, currentGoal: 5 },
+  });
+
+  return createdUser;
 }
